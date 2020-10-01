d25f9fad1505d1cdae663fcf4e86cdbf53e889ac
SilverDragon12 <72190816+SilverDragon12@users.noreply.github.com>
2020-10-01 13:47:41 +0530

    Update Stenographer.java

--- src/isu/apps/stenography/Stenographer.java
+++ src/isu/apps/stenography/Stenographer.java
@@ -5,9 +5,9 @@ public class Stenographer {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.print("Hello World!");
 	}
 
 }
